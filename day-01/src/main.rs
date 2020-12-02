a934dad44772fcedc92d384803053dc8745a9f9b
Parker McMullin <parker.mcmullin01@gmail.com>
2020-12-01 19:08:18 -0700

    Finish day 1!

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,13 +1,57 @@
 use std::fs;
 
-type Result<T> = ::std::result::Result<T, Box<dyn std::error::Error>>;
+const INPUT_FILE: &str = "input/data.txt";
 
-const INPUT: &str = "input/data.txt";
+fn main() {
+    let contents = fs::read_to_string(INPUT_FILE).unwrap();
+    let input: Vec<_> = contents
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap_or(0))
+        .enumerate()
+        .collect();
 
-fn main() -> Result<()> {
-    let contents = fs::read_to_string(INPUT)?;
+    part_one(input.clone());
+    part_two(input.clone());
+}
 
-    dbg!(contents);
+fn part_one(input: Vec<(usize, i32)>) {
+    for (i, i_value) in &input {
+        for (j, j_value) in &input {
+            if i == j {
+                continue;
+            }
 
-    Ok(())
+            if i_value + j_value == 2020 {
+                let answer = i_value * j_value;
+                println!("Answer 1: {}", answer);
+                return ();
+            }
+        }
+    }
+
+    println!("Answer 1 not found!");
+}
+
+fn part_two(input: Vec<(usize, i32)>) {
+    for (i, i_value) in &input {
+        for (j, j_value) in &input {
+            for (k, k_value) in &input {
+                if any_are_the_same(i, j, k) {
+                    continue;
+                }
+
+                if i_value + j_value + k_value == 2020 {
+                    let answer = i_value * j_value * k_value;
+                    println!("Answer 2: {}", answer);
+                    return ();
+                }
+            }
+        }
+    }
+
+    println!("Answer 2 not found!");
+}
+
+fn any_are_the_same(i: &usize, j: &usize, k: &usize) -> bool {
+    i == j || i == k || j == k
 }
